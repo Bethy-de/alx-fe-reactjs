2d2117e4024df64646cd88a833b088afa8e432ea
Bethelhem Degsew Alem <bethydegsew@gmail.com>
2025-11-07 23:53:39 +0300

    Create MainContent.jsx

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+// src/MainContent.jsx
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#e0f7fa', minHeight: '300px' }}>
+      <p>Welcome to the city explorer app!</p>
+    </main>
+  );
+}
+
+export default MainContent;
