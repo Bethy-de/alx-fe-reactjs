c700b8c459a38a3489c37a14cecab604e328f759
Bethelhem Degsew Alem <bethydegsew@gmail.com>
2025-11-07 23:57:09 +0300

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,7 +2,7 @@
 function MainContent() {
   return (
     <main style={{ padding: '20px', backgroundColor: '#e0f7fa', minHeight: '300px' }}>
-      <p>Welcome to the city explorer app!</p>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
