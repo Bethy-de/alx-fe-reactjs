59ac15a78a0882d253821f4ca202649ed08efbc4
Bethelhem Degsew Alem <bethydegsew@gmail.com>
2025-11-08 19:43:37 +0300

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,12 +4,23 @@ import UserProfile from './UserProfile';
 import MainContent from './MainContent';
 import Footer from './Footer';
 import Counter from './components/Counter';
+import React from 'react';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
+
 
 function App() {
   return (
     <div>
       <Header />
       <UserProfile name="Jane Doe" age={28} bio="Loves exploring cities and sharing travel stories." />
+     <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+
+
+
+      
       <MainContent />
       <Counter />
       <Footer />
